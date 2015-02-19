9e7c9c5d4a10bf80c5608b23e860baab916ef89b
Sam Moore <samandmoore@gmail.com>
2015-02-19 10:09:38 -0500

    added sinon and started to set up some tests... i hate node test running

--- backbone.blazer.spec.js
+++ backbone.blazer.spec.js
@@ -5,12 +5,46 @@ var _ = require('underscore'),
 Backbone.$ = $;
 
 var Marionette = require('backbone.marionette'),
-    chai = require('chai'),
-    expect = chai.expect,
     Blazer = require('./backbone.blazer');
 
+var chai = require('chai'),
+    expect = chai.expect,
+    sinon = require('sinon'),
+    sinonChai = require('sinon-chai');
+
+chai.use(sinonChai);
+
 describe('Backbone.Blazer.Router', function() {
+    beforeEach(function() {
+        this.sandbox = sinon.sandbox.create();
+
+        var TestRoute = Backbone.Blazer.Route.extend({
+            render: function() {}
+        });
+
+        var testRoute = this.testRoute = new TestRoute();
+        sinon.spy(this.testRoute, 'render');
+
+        var TestRouter = Backbone.Blazer.Router.extend({
+            routes: {
+                'route': testRoute
+            }
+        });
+        this.router = new TestRouter();
+        sinon.spy(this.router, 'handleRoute');
+
+        window.location = Backbone.history.location = location;
+        Backbone.history.start({ pushState: true });
+    });
+
+    afterEach(function() {
+        Backbone.history.stop();
+        this.sandbox.restore();
+    });
+
     it('should work', function() {
-        expect(true).to.equal(true);
+        history.pushState({}, null, 'http://example.org/route');
+        expect(location.pathname).to.equal('/route');
+
     });
 });
