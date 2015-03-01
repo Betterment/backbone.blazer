69d247acc4476b6f57e51d3a4309ebae7c1cfd96
Sam Moore <samandmoore@gmail.com>
2015-03-01 12:30:45 -0500

    use a sandbox correctly for sinon

--- test/backbone.blazer.spec.js
+++ test/backbone.blazer.spec.js
@@ -7,13 +7,13 @@ describe('Backbone.Blazer.Router', function() {
     var TestRouter = Backbone.Blazer.Router.extend();
 
     beforeEach(function() {
-        this.sandbox = sinon.sandbox.create();
+        this.sinon = sinon.sandbox.create();
 
         this.testRoute = new TestRoute();
 
         this.router = new TestRouter();
         this.router.route('route', this.testRoute);
-        sinon.spy(this.router, 'handleRoute');
+        this.sinon.spy(this.router, 'handleRoute');
 
         Backbone.history.location = new Location('http://example.org');
         Backbone.history.start({ pushState: true });
@@ -21,13 +21,13 @@ describe('Backbone.Blazer.Router', function() {
 
     afterEach(function() {
         Backbone.history.stop();
-        this.sandbox.restore();
+        this.sinon.restore();
     });
 
     it('should process a route correctly', function() {
-        sinon.spy(this.testRoute, 'prepare');
-        sinon.spy(this.testRoute, 'execute');
-        sinon.spy(this.testRoute, 'error');
+        this.sinon.spy(this.testRoute, 'prepare');
+        this.sinon.spy(this.testRoute, 'execute');
+        this.sinon.spy(this.testRoute, 'error');
 
         this.router.navigate('route', { trigger: true });
 
@@ -38,9 +38,9 @@ describe('Backbone.Blazer.Router', function() {
     });
 
     it('should process an error correctly', function() {
-        sinon.stub(this.testRoute, 'prepare', function() { return $.Deferred().reject().promise(); });
-        sinon.spy(this.testRoute, 'execute');
-        sinon.spy(this.testRoute, 'error');
+        this.sinon.stub(this.testRoute, 'prepare', function() { return $.Deferred().reject().promise(); });
+        this.sinon.spy(this.testRoute, 'execute');
+        this.sinon.spy(this.testRoute, 'error');
 
         this.router.navigate('route', { trigger: true });
 
